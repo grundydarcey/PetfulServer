adec6a48393164ffd6a390f31d067d7dd320ce8c
Beyonce Knowles <you@example.com>
2021-02-28 12:32:44 -0500

    changing cors

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://fast-wildwood-90277.herokuapp.com/api/',
+  // CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://fast-wildwood-90277.herokuapp.com/api/',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   PORT: process.env.PORT || 8000,
 };
