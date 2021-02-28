9e7a5330f638f6b08631b456ed4af8e0ce6d4cae
Beyonce Knowles <you@example.com>
2021-02-27 21:20:45 -0500

    fix client origin

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:8000',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://fast-wildwood-90277.herokuapp.com/api/',
   PORT: process.env.PORT || 8000,
 };
