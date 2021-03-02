529cc05039c6bab784a3cf938b5356ccdfe4a520
Beyonce Knowles <you@example.com>
2021-03-02 14:01:38 -0500

    fix

--- modules/people/people.router.js
+++ modules/people/people.router.js
@@ -11,8 +11,8 @@ peopleRouter
     res.status(200).json(allPeople);
   })
   .post(json, (req, res) => {
-    const { name } = req.body;
-    const data = { name };
+    const { value } = req.body;
+    const data = { value };
     if (!data) 
       return res.status(400).json({
         error: 'Name is required to adopt'
