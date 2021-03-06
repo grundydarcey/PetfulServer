d8a82e0ef66587c0c042783235f6ab2f2f3b9d9b
Beyonce Knowles <you@example.com>
2021-03-06 14:04:05 -0500

    restart

--- modules/pets/pets.router.js
+++ modules/pets/pets.router.js
@@ -6,8 +6,8 @@ const petsRouter = express.Router();
 petsRouter
   .route('/')
   .get((req, res) => {
-    const allPeets = Pets.get();
-    res.status(200).json(allPeets);
+    const allPets = Pets.get();
+    res.status(200).json(allPets);
   })
   .delete(json, (req, res) => {
     const { type } = req.body;
