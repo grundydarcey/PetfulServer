4e7700ac8e84d2a5047da0f20bd2bbba6854393c
Beyonce Knowles <you@example.com>
2021-03-06 18:49:33 -0500

    push

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   // CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://fast-wildwood-90277.herokuapp.com/api/',
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://petful-client-template-grundydarcey.vercel.app',
   PORT: process.env.PORT || 8000,
 };
