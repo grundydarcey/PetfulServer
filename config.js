7362a2d219bd774ca09138bbcf8e1c989487df14
Beyonce Knowles <you@example.com>
2021-03-21 14:14:18 -0400

    client origin update

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://petful-client-template-grundydarcey.vercel.app',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://petful-client-template-979vllh0s-grundydarcey.vercel.app/',
   PORT: process.env.PORT || 8000,
 };
